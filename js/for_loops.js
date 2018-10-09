4ab834cc09f5ea9a0bc108cdd8a6ce32aaea4c9a
Keenan Smith <keenansmith1965@gmail.com>
2018-10-09 17:25:39 -0500

    merging for first grade

---
+++ js/for_loops.js
@@ -0,0 +1,64 @@
+"use strict";
+// Create a function named showMultiplicationTable that accepts a number
+// and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
+//
+// For example, showMultiplicationTable(7) should output
+
+// 7 x 1 = 7
+// 7 x 2 = 14
+// 7 x 3 = 21
+// 7 x 4 = 28
+// 7 x 5 = 35
+// 7 x 6 = 42
+// 7 x 7 = 49
+// 7 x 8 = 56
+// 7 x 9 = 63
+// 7 x 10 = 70
+
+
+// function showMultiplicationTable(workingNum){
+//     for (var increNum = 1; increNum <= 10; increNum++) {
+//         console.log(workingNum + " x " + increNum + " = " + (workingNum * increNum));
+//     }
+// }
+// showMultiplicationTable(7);
+
+// for (var decreNum = 100; decreNum >= 5; decreNum -= 5) {
+//     console.log(decreNum);
+// }
+
+// var number = 2;
+// // // while(number <= 65536){
+// // //     num *= 2;
+// // //     console.log(number);
+// // //
+
+// console.log("It's a great day at the ice cream selling stand!");
+//
+// var allCones = Math.floor(Math.random() * 50) + 50;
+// console.log("I have" + allCones + "number of cones to sell everybody!");
+//
+// var conesOrder;
+// do {
+//     conesOrder = Math.ceil(Math.random() * 5);
+//     if (conesOrder <= allCones){
+//     allCones - allCones - conesOrdered;
+//     console.log("I now have" + allCones + "left.");
+// } else {
+//     console.log("Not enough for you. Next customer, please!");
+// } while(allCones > 0);
+//     console.log("Yay! I sold all the cones and get to go home!");}
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+var randomNumber;
+for (var 1 = 0; i < 10; i++) {
+    randomNumber = Math.floor(Math.random() * 180) + 20;
+
+    if(isEven(randomNumber)){
+        console.log(randomNumber + " is even!");
+    } else {
+        console.log(randomNumber + " is odd!");
+    }
+}
---
+++ js/while.js
@@ -0,0 +1,26 @@
+"use strict";
+//doubling numbers
+//starting with 2
+//ending with 65536
+
+// var num = 2;
+// while (num <= 65536) {
+//     console.log(num);
+//     num = num * 2;
+// }
+
+//doubling numbers
+//starting with 10
+//ending in -2
+
+// var num = 10;
+// while (num <= -2) {
+//     console.log(num);
+//     num = num - 2;
+// }
+
+for (var i = 1; 1 <= 10; i +=1){
+    if (i ===5) {
+
+    }
+}
