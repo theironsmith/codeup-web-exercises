17e1f3a08d72756960973122462d21b1c658cf61
Keenan Smith <keenansmith1965@gmail.com>
2018-10-15 12:04:15 -0500

    DOM exercise completed, ready for running and gunning

---
+++ js/Basics-Drills.js
@@ -0,0 +1,44 @@
+var i = 10;
+
+while (i <= 10){
+    console.log("They are the same")
+}
+
+var i = 10;
+
+while (i <= 10){
+    console.log("They are the same")
+}
+//---------------while loop--------------------------------------
+var title = document.getElementById("main-title");
+console.log(title.innerHTML);
+
+var title = document.getElementById("main-title");
+console.log(title.innerText);
+//------------------DOM-----------------------------------
+
+for (/*initialization*/; /*condition*/; /*increment*/) {
+    // body
+}
+
+for (var i = 0; i < 10; i++) {
+    console.log('for loop iteration #' + i);
+}
+
+for (var i = 7; i < 40; i++){
+    console.log("for loop iteration #" + i);
+}
+//--------for loops---------------------------------------
+
+// if/else example
+if (condition) {
+    // code here gets executed if condition evaluates to true
+} else {
+    // code here gets executed if condition evaluates to false
+}
+
+if(raining) {
+    alert("bring umbrella")
+} else {
+    alert("enjoy the sunny day!")
+}
